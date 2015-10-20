c6589baa0defbd3a1cfe9c01410c2ff6b2eb71d8
ellenlt <ellenltsay@gmail.com>
2015-10-20 10:18:11 -0700

    Buggy version of Problem 4.1 Part 1, Logistic Regression (have problem with mismatched rows in test data using predict function)

--- HW1/otherclassificationmodels.R
+++ HW1/otherclassificationmodels.R
@@ -3,15 +3,44 @@ getwd()
 library(LiblineaR)
 library(randomForest)
 
+# Load raw data from .csv file
 trainingData <- read.csv("./training.csv", header=TRUE, as.is=TRUE)
 testData <- read.csv("./test.csv", header=TRUE, as.is=TRUE)
 diseaseName <- "Obesity"
-wordsInVocab <<- 10582
+
+# Convert our features of interest to a matrix
+X <- do.call(rbind, trainingData[1:wordsInVocab])
+X <- t(X)
+X <- subset(X, select=c("obese","obesity","morbid","sleep","apnea","albuterol","obstructive","wheezing","chronic","complications","morbidly","allergy","bowel","study"))
+dfX <- data.frame(X)
+
+# Convert test data to a dataframe
+newX <- do.call(rbind, testData[1:wordsInVocab])
+newX <- t(newX)
+newX <- subset(newX, select=c("obese","obesity","morbid","sleep","apnea","albuterol","obstructive","wheezing","chronic","complications","morbidly","allergy","bowel","study"))
+newX <- data.frame(matrix(unlist(testData[1:wordsInVocab]), nrow=length(testData[,1]), byrow=F),stringsAsFactors=F)
+newX <- data.frame(X)
+
+# Convert our disease of interest into a binary vector
+y <- trainingData[,diseaseName]
+y[y=="Y"] <- 1
+y[y=="N"] <- 0
+class(y)<- "numeric"
+
+# Mean absolute error
+mae <- function(errors) { mean(abs(errors)) }
 
 # Logistic regression
-glmModel <- glm(y ~ X, family="binomial")
-glmTrainingPredictions <- predict(glmModel)
-glmTestingPredictions <- predict(glmModel, testdata)
+glmModel <- glm(formula = y ~ X, family="binomial")
+summary(glmModel)
+# Training error
+glmTrainingPredictions <- predict(glmModel, se.fit=T)
+glmTrainingError <- mae(glmTrainingPredictions$se.fit)
+glmTrainingError
+# Test error
+glmTestingPredictions <- predict(glmModel, newX, se.fit=T)
+glmTestingError <- mae(glmTestingPredictions$se.fit)
+glmTestingError
 
 # Support vector machine
 # Create matrix testData and trainData with data
@@ -25,21 +54,3 @@ rfModel <- randomForest(trainData, y, ntree=500, do.trace=T)
 rfTrainingPredictions <- predict(rfModel)
 rfTestingPredictions <- predict(rfModel, testData)
 rfImportantFeatures <- rfModel$importance
-
-# filterFeatures
-# Input: Data as a list
-# Output: Data frame containing columns for all the words as well as the disease of interest.
-# Returned data frame doesn't include columns for other diseases
-filterFeatures <- function(data, disease) {
-  words <- data.frame(matrix(unlist(data[1:wordsInVocab]), nrow=length(data[,1]), byrow=F),stringsAsFactors=F)
-  names(words) <- names(data[1:wordsInVocab])
-  words$ID <- 1:nrow(words)
-  
-  disease <- data.frame(unlist(data[,diseaseName]),stringsAsFactors=F)
-  names(disease) <- diseaseName
-  disease$ID <- 1:nrow(disease)
-  
-  dataTable <- merge(disease,words,by="ID")
-  dataTable$ID <- NULL
-  dataTable
-}
